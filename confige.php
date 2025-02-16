6745b0fa4bc9ca253cfc2a50cba80538c0f683c5
Mohit <135161447+Mohitourya6263@users.noreply.github.com>
2025-02-16 17:32:44 +0530

    Create confige.php

---
+++ confige.php
@@ -0,0 +1,15 @@
+<?php
+ $servername="localhost";
+ $username="root";
+ $password="";
+ $dbname="jalwala_db";
+  $conn=mysqli_connect($servername,$username,$password,$dbname);
+  if(!$conn)
+  {
+    echo"connection failed";
+  }
+  else
+  {
+    echo " connection is successfully";
+  }
+  ?>
